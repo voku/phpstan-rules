ba282e297e0b3e29e4e3d53f96f7fa326ef18d2f
Lars Moelleken <lmoelleken@meerx.com>
2022-07-23 05:35:01 +0200

    [~]: testing only

--- tests/IfConditionRuleTest.php
+++ tests/IfConditionRuleTest.php
@@ -70,11 +70,11 @@ final class IfConditionRuleTest extends RuleTestCase
                     37,
                 ],
                 [
-                  'BooleanAnd: Use a method to check the condition e.g. `$foo->value()` instead of `$foo`.',
-                  44  
+                    'BooleanAnd: Use a method to check the condition e.g. `$foo->value()` instead of `$foo`.',
+                    44
                 ],
                 [
-                    'SmallerOrEqual: Do not compare objects directly, DateTimeImmutable and \'2032-03-04\' found.', 
+                    'SmallerOrEqual: Do not compare objects directly, DateTimeImmutable and \'2032-03-04\' found.',
                     49
                 ],
                 [
@@ -83,15 +83,15 @@ final class IfConditionRuleTest extends RuleTestCase
                 ],
                 [
                     'Equal: Do not compare objects directly, DateTimeImmutable and \'2032-03-04\' found.',
-                    55  
+                    55
                 ],
                 [
-                  'SmallerOrEqual: Do not compare objects directly, DateTimeImmutable and \'2032-03-04\' found.',
-                    72  
+                    'SmallerOrEqual: Do not compare objects directly, DateTimeImmutable and \'2032-03-04\' found.',
+                    72
                 ],
                 [
                     'Smaller: Do not compare objects directly, DateTimeImmutable|null and \'2013-04-05\' found.',
-                    87, 
+                    87,
                 ],
                 [
                     'Smaller: Do not compare objects directly, DateTimeImmutable|null and DateTimeImmutable found.',
@@ -108,134 +108,6 @@ final class IfConditionRuleTest extends RuleTestCase
                 [
                     'BooleanAnd: Do not compare boolean and integer.',
                     122,
-                ],
-                [
-                    'Equal: Insane comparison between 0.0 and \'\'',
-                    148,
-                ],
-                [
-                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
-                    148,
-                ],
-                [
-                    'Equal: Insane comparison between \'\' and 0.0',
-                    148,
-                ],
-                [
-                    'NotEqual: Please do not use empty-string check for numeric values. e.g. `0 != \'\'` is not working with >= PHP 8.',
-                    152,    
-                ],
-                [
-                    'NotEqual: Possible insane comparison between \'\' and 0.0',
-                    152,
-                ],
-                [
-                    'Equal: Insane comparison between 0 and \'\'',
-                    156,
-                ],
-                [
-                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
-                    156,    
-                ],
-                [
-                    'Equal: Insane comparison between \'\' and 0',
-                    156,    
-                ],
-                [
-                    'NotEqual: Please do not use empty-string check for numeric values. e.g. `0 != \'\'` is not working with >= PHP 8.',
-                    160,
-                ],
-                [
-                    'NotEqual: Please do not use double negative integer conditions. e.g. `(int)$foo != 0` is the same as `(int)$foo`.',
-                    160,
-                ],
-                [
-                    'NotEqual: Possible insane comparison between \'\' and 0',
-                    160,
-                ],
-                [
-                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
-                    164,
-                ],
-                [
-                    'Equal: Insane comparison between \'\' and 0',
-                    164,
-                ],
-                [
-                    'Equal: Insane comparison between 0 and \'\'',
-                    164,
-                ],
-                [
-                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
-                    168,
-                ],
-                [
-                    'Equal: Possible insane comparison between \'\' and 0|null',
-                    168,
-                ],
-                [
-                    'Equal: Insane comparison between 0 and \'0foo\'',
-                    175,
-                ],
-                [
-                    'Equal: Insane comparison between \'0foo\' and 0',  
-                    175,
-                ],
-                [
-                    'Identical: Insane comparison between 0 and \'0foo\'',
-                    178,
-                ],
-                [
-                    'Identical: Insane comparison between \'0foo\' and 0',
-                    178,
-                ],
-                [
-                    'NotEqual: Possible insane comparison between \'0foo\' and 1',
-                    181,
-                ],
-                [
-                    'Equal: Possible insane comparison between \'3\' and true',
-                    184,
-                ],
-                [
-                    'Equal: Do not compare boolean and string.',
-                    184,
-                ],
-                [
-                    'Equal: Possible insane comparison between \'0.000\' and 3|null',
-                    196,
-                ],
-                [
-                    'Equal: Possible insane comparison between null and 0|3',
-                    204,
-                ],
-                [
-                    'NotEqual: Insane comparison between 1 and \'1\'',
-                    207,
-                ],
-                [
-                    'NotEqual: Insane comparison between \'1\' and 1',
-                    207,
-                ],
-                [
-                    'NotIdentical: Insane comparison between 1 and 1',
-                    210,
-                ],
-                [
-                    'NotIdentical: Insane comparison between 1 and 1',
-                    210,
-                ],
-                [
-                    'Equal: Possible insane comparison between \'0\' and 0',
-                    213,
-                ],
-                [
-                    'Identical: Insane comparison between 0 and \'0\'',
-                    216,
-                ],
-                [
-                    'Identical: Insane comparison between \'0\' and 0',
-                    216,
                 ],
             ]
         );
@@ -258,6 +130,282 @@ final class IfConditionRuleTest extends RuleTestCase
                 [
                     'Greater: Do not compare objects directly, voku\PHPStan\Rules\Test\fixtures\BulbOn::OFF|voku\PHPStan\Rules\Test\fixtures\BulbOn::ON and voku\PHPStan\Rules\Test\fixtures\BulbOn::ON found.',
                     12
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @requires PHP 8.0
+     */
+    public function testIfConditions80(): void
+    {
+        $this->analyse(
+            [
+                __DIR__ . '/fixtures/IfConditionsFixtures80.php',
+            ],
+            [
+                [
+                    'Equal: Insane comparison between 0.0 and \'\'',
+                    7,
+                ],
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    7,
+                ],
+                [
+                    'Equal: Insane comparison between \'\' and 0.0',
+                    7,
+                ],
+                [
+                    'NotEqual: Please do not use empty-string check for numeric values. e.g. `0 != \'\'` is not working with >= PHP 8.',
+                    11,
+                ],
+                [
+                    'NotEqual: Possible insane comparison between \'\' and 0.0',
+                    11,
+                ],
+                [
+                    'Equal: Insane comparison between 0 and \'\'',
+                    15,
+                ],
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    15,
+                ],
+                [
+                    'Equal: Insane comparison between \'\' and 0',
+                    15,
+                ],
+                [
+                    'NotEqual: Please do not use empty-string check for numeric values. e.g. `0 != \'\'` is not working with >= PHP 8.',
+                    19,
+                ],
+                [
+                    'NotEqual: Please do not use double negative integer conditions. e.g. `(int)$foo != 0` is the same as `(int)$foo`.',
+                    19,
+                ],
+                [
+                    'NotEqual: Possible insane comparison between \'\' and 0',
+                    19,
+                ],
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    23,
+                ],
+                [
+                    'Equal: Insane comparison between \'\' and 0',
+                    23,
+                ],
+                [
+                    'Equal: Insane comparison between 0 and \'\'',
+                    23,
+                ],
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    27,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'\' and 0|null',
+                    27,
+                ],
+                [
+                    'Equal: Insane comparison between 0 and \'0foo\'',
+                    34,
+                ],
+                [
+                    'Equal: Insane comparison between \'0foo\' and 0',
+                    34,
+                ],
+                [
+                    'Identical: Insane comparison between 0 and \'0foo\'',
+                    37,
+                ],
+                [
+                    'Identical: Insane comparison between \'0foo\' and 0',
+                    37,
+                ],
+                [
+                    'NotEqual: Possible insane comparison between \'0foo\' and 1',
+                    40,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'3\' and true',
+                    43,
+                ],
+                [
+                    'Equal: Do not compare boolean and string.',
+                    43,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'0.000\' and 3|null',
+                    55,
+                ],
+                [
+                    'Equal: Possible insane comparison between null and 0|3',
+                    63,
+                ],
+                [
+                    'NotEqual: Insane comparison between 1 and \'1\'',
+                    66,
+                ],
+                [
+                    'NotEqual: Insane comparison between \'1\' and 1',
+                    66,
+                ],
+                [
+                    'NotIdentical: Insane comparison between 1 and 1',
+                    69,
+                ],
+                [
+                    'NotIdentical: Insane comparison between 1 and 1',
+                    69,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'0\' and 0',
+                    72,
+                ],
+                [
+                    'Identical: Insane comparison between 0 and \'0\'',
+                    75,
+                ],
+                [
+                    'Identical: Insane comparison between \'0\' and 0',
+                    75,
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @requires PHP < 8.0
+     */
+    public function testIfConditions74(): void
+    {
+        $this->analyse(
+            [
+                __DIR__ . '/fixtures/IfConditionsFixtures74.php',
+            ],
+            [
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    7,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'\' and 0.0',
+                    7,
+                ],
+                [
+                    'NotEqual: Insane comparison between 0.0 and \'\'',
+                    11
+                ],
+                [
+                    'NotEqual: Please do not use empty-string check for numeric values. e.g. `0 != \'\'` is not working with >= PHP 8.',
+                    11,
+                ],
+                [
+                    'NotEqual: Insane comparison between \'\' and 0.0',
+                    11,
+                ],
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    15,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'\' and 0',
+                    15,
+                ],
+                [
+                    'NotEqual: Insane comparison between 0 and \'\'',
+                    19,
+                ],
+                [
+                    'NotEqual: Please do not use empty-string check for numeric values. e.g. `0 != \'\'` is not working with >= PHP 8.',
+                    19,
+                ],
+                [
+                    'NotEqual: Please do not use double negative integer conditions. e.g. `(int)$foo != 0` is the same as `(int)$foo`.',
+                    19,
+                ],
+                [
+                    'NotEqual: Insane comparison between \'\' and 0',
+                    19,
+                ],
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    23,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'\' and 0',
+                    23,
+                ],
+                [
+                    'Equal: Please do not use empty-string check for numeric values. e.g. `0 == \'\'` is not working with >= PHP 8.',
+                    27,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'\' and 0|null',
+                    27,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'0foo\' and 0',
+                    34,
+                ],
+                [
+                    'Identical: Insane comparison between 0 and \'0foo\'',
+                    37,
+                ],
+                [
+                    'Identical: Insane comparison between \'0foo\' and 0',
+                    37,
+                ],
+                [
+                    'NotEqual: Possible insane comparison between \'0foo\' and 1',
+                    40,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'3\' and true',
+                    43,
+                ],
+                [
+                    'Equal: Do not compare boolean and string.',
+                    43,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'0.000\' and 3|null',
+                    55,
+                ],
+                [
+                    'Equal: Possible insane comparison between null and 0|3',
+                    63,
+                ],
+                [
+                    'NotEqual: Insane comparison between 1 and \'1\'',
+                    66,
+                ],
+                [
+                    'NotEqual: Insane comparison between \'1\' and 1',
+                    66,
+                ],
+                [
+                    'NotIdentical: Insane comparison between 1 and 1',
+                    69,
+                ],
+                [
+                    'NotIdentical: Insane comparison between 1 and 1',
+                    69,
+                ],
+                [
+                    'Equal: Possible insane comparison between \'0\' and 0',
+                    72,
+                ],
+                [
+                    'Identical: Insane comparison between 0 and \'0\'',
+                    75,
+                ],
+                [
+                    'Identical: Insane comparison between \'0\' and 0',
+                    75,
                 ],
             ]
         );
