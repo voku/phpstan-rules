cca3b7d007675ea197dbe0ca31046a13c283961d
Lars Moelleken <lars@moelleken.org>
2026-08-28 21:52:05 +0200

    fix: defer extended assign diagnostics in traits

--- src/voku/PHPStan/Rules/ExtendedAssignOpRule.php
+++ src/voku/PHPStan/Rules/ExtendedAssignOpRule.php
@@ -8,10 +8,6 @@ use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Rules\Rule;
-use PHPStan\Rules\RuleErrorBuilder;
-use PHPStan\Type\ErrorType;
-use PHPStan\Type\VerbosityLevel;
-use function sprintf;
 
 /**
  * @implements Rule<Node\Expr>
@@ -19,31 +15,20 @@ use function sprintf;
 class ExtendedAssignOpRule implements Rule
 {
     /**
-     * @var bool
+     * @var ExtendedAssignOpRuleDiagnostics
      */
-    private $checkForAssignments;
-
-    /**
-     * @var bool
-     */
-    private $checkYodaConditions;
-
-    /**
-     * @var ReflectionProvider
-     */
-    private $reflectionProvider;
+    private $diagnostics;
 
     public function __construct(
         ReflectionProvider $reflectionProvider,
         bool $checkForAssignments = false,
         bool $checkYodaConditions = false
-    )
-    {
-        $this->reflectionProvider = $reflectionProvider;
-
-        $this->checkForAssignments = $checkForAssignments;
-
-        $this->checkYodaConditions = $checkYodaConditions;
+    ) {
+        $this->diagnostics = new ExtendedAssignOpRuleDiagnostics(
+            $reflectionProvider,
+            $checkForAssignments,
+            $checkYodaConditions
+        );
     }
 
     public function getNodeType(): string
@@ -58,107 +43,10 @@ class ExtendedAssignOpRule implements Rule
      */
     public function processNode(Node $node, Scope $scope): array
     {
-        $errors = [];
-
-        $leftType = $scope->getType($node->var);
-        $rightType = $scope->getType($node->expr);
-
-        $errors = IfConditionHelper::processNodeHelper(
-            $leftType,
-            $rightType,
-            $node,
-            $errors,
-            [],
-            $node,
-            $this->reflectionProvider,
-            $this->checkForAssignments,
-            $this->checkYodaConditions
-        );
-        $errors = IfConditionHelper::processNodeHelper(
-            $rightType,
-            $leftType,
-            $node,
-            $errors,
-            [],
-            $node,
-            $this->reflectionProvider,
-            false,
-            false
-        );
-
-        $errorsFound = false;
-        $this->checkErrors($node, $leftType, $rightType, $errors, $errorsFound);
-        if ($errorsFound === false) {
-            $this->checkErrors($node, $rightType, $leftType, $errors, $errorsFound);
+        if ($scope->isInTrait()) {
+            return [];
         }
 
-        return $errors;
-    }
-
-    /**
-     * @param Node\Expr\AssignOp $node
-     * @param array<int, \PHPStan\Rules\RuleError> $errors
-     */
-    private function checkErrors(
-        Node\Expr          $node,
-        \PHPStan\Type\Type $type_1,
-        \PHPStan\Type\Type $type_2,
-        array              &$errors,
-        bool               &$errorsFound
-    ): void
-    {
-        if (
-            $type_1->isString()->yes()
-            &&
-            !($type_2 instanceof \PHPStan\Type\MixedType)
-            &&
-            !IfConditionHelper::isPhpStanTypeMaybeWithUnionNullable($type_2, \PHPStan\Type\StringType::class)
-            &&
-            $type_2->describe(VerbosityLevel::typeOnly()) !== 'string' // INFO: hack for non-empty-string
-        ) {
-            if (
-                $node instanceof Node\Expr\AssignOp\Concat
-                &&
-                !IfConditionHelper::hasConstantStringValue($type_1, '')
-                &&
-                !($type_2->toString() instanceof ErrorType)
-            ) {
-                return;
-            }
-
-            $errors[] = IfConditionHelper::buildErrorMessage(
-                $node,
-                sprintf(
-                    'string (%s) in combination with non-string (%s) is not allowed.',
-                    $type_1->describe(VerbosityLevel::value()),
-                    $type_2->describe(VerbosityLevel::value())
-                ),
-                $node->getStartLine()
-            );
-
-            $errorsFound = true;
-
-            return;
-        }
-
-        if (
-            $type_1->isArray()->yes()
-            &&
-            !($type_2 instanceof \PHPStan\Type\MixedType)
-            &&
-            $type_2->isArray()->no()
-        ) {
-            $errors[] = IfConditionHelper::buildErrorMessage(
-                $node,
-                sprintf(
-                    'array (%s) in combination with non-array (%s) is not allowed.',
-                    $type_1->describe(VerbosityLevel::value()),
-                    $type_2->describe(VerbosityLevel::value())
-                ),
-                $node->getStartLine()
-            );
-
-            $errorsFound = true;
-        }
+        return $this->diagnostics->processNode($node, $scope);
     }
 }
